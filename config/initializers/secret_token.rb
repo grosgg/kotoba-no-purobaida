0fec1b2d2a478f63282eca8bc0ddfb70faf3b54f
Jeremy Marin <jeremy@adikteev.com>
2013-11-12 18:23:34 +0100

    Rails 4 + MongoID + Rspec + Cucumber Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KotobaNoPurobaida::Application.config.secret_key_base = 'baffd3cb8429c5e2db52fcd850ecbaab18933fffb98ffb19edaeea6047eab14e5ce3d87b57e08fbfb611d249261f9b097b3c0dd48e2d91fd20ca1de015cd4240'
