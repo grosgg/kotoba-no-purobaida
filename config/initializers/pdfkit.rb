1b2a237a0c4ebf1bed683afdd055136c99e7bd43
Jeremy Marin <marin.jeremy@gmail.com>
2013-11-30 09:37:45 +0100

    Add japanese font

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -6,4 +6,14 @@ PDFKit.configure do |config|
     raise "Unsupported. Must be running linux or intel-based Mac OS."
   end
   config.wkhtmltopdf = Rails.root.join('vendor', 'bin', wkhtmltopdf_executable).to_s
+
+  if Rails.env.production?
+    font_dir = File.join(Dir.home, ".fonts")
+    Dir.mkdir(font_dir) unless Dir.exists?(font_dir)
+
+    Dir.glob(Rails.root.join("vendor", "fonts", "*")).each do |font|
+      target = File.join(font_dir, File.basename(font))
+      File.symlink(font, target) unless File.exists?(target)
+    end
+  end
 end
