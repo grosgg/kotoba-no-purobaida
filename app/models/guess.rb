1c5ca0f5b6d54ee6e5f5f66dee658a69dcd52483
Jeremy Marin <marin.jeremy@gmail.com>
2013-12-15 23:32:40 +0100

    Improve Guesses display on PDF

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -22,7 +22,7 @@ class Guess < Exercice
       words = words.where(:tags.in => @tags)
     end
 
-    words = words.to_a.shuffle.take(25 * @pages)
+    words = words.to_a.shuffle.take(28 * @pages)
 
     word_grid = []
     words.each do |w|
