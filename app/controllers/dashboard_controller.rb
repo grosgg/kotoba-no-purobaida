bd74f9c5172ffc0c9ca008db1af92872433037bd
Jeremy Marin <marin.jeremy@gmail.com>
2013-12-19 00:28:43 +0100

    Fix words order on dashboard ref to #5

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,9 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @recent_kanji = Word.where(:user => current_user).with_kanji.limit(10)
-    @recent_katakana = Word.where(:user => current_user).with_katakana.limit(10)
-    @recent_hiragana = Word.where(:user => current_user).with_hiragana.without_kanji.limit(10)
+    @recent_kanji = Word.where(:user => current_user).with_kanji.order_by(:id => :desc).limit(10)
+    @recent_katakana = Word.where(:user => current_user).with_katakana.order_by(:id => :desc).limit(10)
+    @recent_hiragana = Word.where(:user => current_user).with_hiragana.without_kanji.order_by(:id => :desc).limit(10)
 
     @popular_tags = Tag.where(:user => current_user).order_by(:count => :desc).limit(20)
   end
