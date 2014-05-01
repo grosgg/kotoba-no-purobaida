6c4dc864b1040f7c0c6b36bdd28f3a36f3a4c303
Jeremy Marin <marin.jeremy@gmail.com>
2014-05-01 19:21:01 +0200

    Fix scopes on Word

--- app/models/word.rb
+++ app/models/word.rb
@@ -17,25 +17,25 @@ class Word
   paginates_per 10
   search_in :english, :french
 
-  scope :with_english, where(
+  scope :with_english, -> { where(
     :english.ne => '', :english.exists => true
-  )
-  scope :with_french, where(
+  )}
+  scope :with_french, -> { where(
     :french.ne => '', :french.exists => true
-  )
-  scope :with_kanji, where(
+  )}
+  scope :with_kanji, -> { where(
     :kanji.ne => '', :kanji.exists => true
-  )
-  scope :with_hiragana, where(
+  )}
+  scope :with_hiragana, -> { where(
     :hiragana.ne => '', :hiragana.exists => true
-  )
-  scope :with_katakana, where(
+  )}
+  scope :with_katakana, -> { where(
     :katakana.ne => '', :katakana.exists => true
-  )
-  scope :without_kanji, any_of(
+  )}
+  scope :without_kanji,  -> { any_of(
     {:kanji.exists => false},
     {:kanji => ''}
-  )
+  )}
 
   def tags_list=(arg)
     self.tags = arg.split(',').map { |v| v.strip }
