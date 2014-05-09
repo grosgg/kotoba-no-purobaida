2f551e735b9e2258e1788b384536a12675d38d72
Jeremy Marin <marin.jeremy@gmail.com>
2014-05-10 01:21:52 +0200

    Make config min safe

--- app/assets/javascripts/quiz/app.js
+++ app/assets/javascripts/quiz/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('KotobaApp',['quizServices']);
 
-app.config(function($httpProvider) { 
+app.config(['$httpProvider', function($httpProvider) { 
   authToken = $("meta[name=\"csrf-token\"]").attr("content");
   $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
-});
+}]);
